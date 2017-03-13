36253697cb19f0a44e78c8c75bd24c22949cdcfe
Ziheng ZHANG <asdfghjkl951025@gmail.com>
2017-03-13 14:06:29 +0000

    Version 2.0 13th March upload

--- Eight copyists/Engineers.java
+++ Eight copyists/Engineers.java
@@ -1,5 +1,5 @@
 // Name: Ziheng ZHANG;  ID: 201220030
-package Engineers;
+// Version 2.0
 
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
@@ -7,11 +7,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 class Copyist extends Thread {
-
     private Drawing left;
     private Drawing right;
     private int id;
     private int ponderFactor;
+    private boolean drawEd;
     private Random rand = new Random(47);
 
     public Copyist(Drawing left, Drawing right, int id, int ponderFactor) {
@@ -19,6 +19,7 @@ class Copyist extends Thread {
         this.right = right;
         this.id = id;
         this.ponderFactor = ponderFactor;
+        this.drawEd = false;
     }
 
     private void pause() throws InterruptedException {
@@ -30,20 +31,22 @@ class Copyist extends Thread {
 
     public void run() {
         try {
-            while (!Thread.interrupted()) {
-                System.out.println(this + " " + "waiting");
+            while ((!Thread.interrupted())&&(!drawEd)) {
+                System.out.println(this + " is waiting.");
                 pause();
-                System.out.println(this + " " + "grabbing right");
+                System.out.println(this + " is grabbing right.");
                 right.take();
-                System.out.println(this + " " + "grabbing left");
+                System.out.println(this + " is grabbing left.");
                 left.take();
-                System.out.println(this + " " + "drawing");
+                System.out.println(this + " is drawing.");
                 pause();
                 right.drop();
                 left.drop();
+                System.out.println(this + " has completed drawing.");
+                drawEd = true;
             }
         } catch (InterruptedException e) {
-            System.out.println(this + " " + "exiting via interrupt");
+            System.out.println(this + " exiting via interrupt");
         }
     }
 
@@ -53,7 +56,6 @@ class Copyist extends Thread {
 }
 
 class Drawing {
-
     private boolean taken = false;
 
     public synchronized void take() throws InterruptedException {
@@ -72,7 +74,7 @@ class Drawing {
 public class Engineers {
 
     public static void main(String[] args) throws Exception {
-        int ponder = 0;
+        int ponder = 10;
         int size = 8;
         ExecutorService exec = Executors.newCachedThreadPool();
         Drawing[] sticks = new Drawing[size];
