fcc623ace703f7a20a1d96c2505f04a6d46b3965
Ziheng ZHANG <asdfghjkl951025@gmail.com>
2017-03-14 21:33:40 +0000

    Final version 14th March upload

--- Eight copyists/Engineers.java
+++ Eight copyists/Engineers.java
@@ -1,38 +1,42 @@
-// Name: Ziheng ZHANG;  ID: 201220030
-// Version 2.0
-package Engineers;
+// Name: Ziheng ZHANG;  ID: 201220030;  CS-ID: x6zz
+// Version 4.0 (Final version)
 
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
+// Copyist class which extends Thread
 class Copyist extends Thread {
-    private Pencil pencil;
-    private Eidograph eidograph;
-    private int id;
-    private int ponderFactor;
-    private int drawing;
-    private Random rand = new Random(100);
-
+    // six attributes belonging to each copyist
+    private final Pencil pencil; // the pencil next to him/her
+    private final Eidograph eidograph; // the eidograph next to him/her
+    private final int id; // his/her ID
+    private final int ponderFactor; // puase factor (default = 1)
+    private final Random rand = new Random(); // random time for his copying and checking
+    private int drawing; // number of his copys
+    
+    // constructor
     public Copyist(Pencil pencil, Eidograph eidograph, int id, int ponderFactor) {
         this.pencil = pencil;
         this.eidograph = eidograph;
         this.id = id;
         this.ponderFactor = ponderFactor;
-        this.drawing = 1;
+        this.drawing = 1; // default begin with 1
     }
-
+    
+    // pause function
     private void pause() throws InterruptedException {
         if (ponderFactor == 0) {
             return;
         }
         TimeUnit.MILLISECONDS.sleep(rand.nextInt(ponderFactor * 250));
     }
-
+    
+    // run function to control the whole process of one copyist (thread)
     public void run() {
         try {
-            while ((!Thread.interrupted())&&(drawing < 6)) {
+            while ((!Thread.interrupted()) && (drawing < 6)) {
                 System.out.println(this + " is waiting.");
                 pause();
                 System.out.println(this + " is using pencil.");
@@ -46,113 +50,108 @@ class Copyist extends Thread {
                 System.out.println(this + " has finished with eidograph.");
                 checking();
                 drawing++;
-            }
+            } // while thread available and his/her copys < 6
             System.out.println(this + " has finished work and gone to pub.");
         } catch (InterruptedException e) {
             System.out.println(this + " exiting via interrupt");
-        }
+        } // Otherwise exit with interrupt
     }
     
+    // copying function with specified random time range
     // ODD copyist 30 <= Copying <= 80
     // EVEN copyist 30 <= Copying <= 60
-    public void copying(){
+    public void copying() {
         System.out.println(this + " is making drawing.");
-        try{
-            if((this.id & 2) == 0){
-                sleep(rand.nextInt(60-30));
-            }else{
-                sleep(rand.nextInt(80-30));
+        try {
+            if ((this.id & 2) == 0) {
+                sleep(rand.nextInt(60 - 30));
+            } else {
+                sleep(rand.nextInt(80 - 30));
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
     
+    // checking function with specified random time range
     // ODD copyist 40 <= Checking <= 60
     // EVEN copyist 40 <= Checking <= 100
-    public void checking(){
+    public void checking() {
         System.out.println(this + " has finished copy " + this.drawing);
         System.out.println(this + " is checking copy " + this.drawing);
-        try{
-            if((this.id & 2) == 0){
-                sleep(rand.nextInt(100-40));
-            }else{
-                sleep(rand.nextInt(60-40));
+        try {
+            if ((this.id & 2) == 0) {
+                sleep(rand.nextInt(100 - 40));
+            } else {
+                sleep(rand.nextInt(60 - 40));
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    
+
     // Override toString
     public String toString() {
         return "Copyist " + id;
     }
 }
 
-class Pencil{
-    private boolean takenPencil = false;
+// Pencil class and its synchronization
+class Pencil {
+    private boolean takenPencil = false; // indicating pencil state (taken or not)
     public synchronized void take() throws InterruptedException {
         while (takenPencil) {
-            wait();
+            wait(); // taking some time to get access
         }
-        takenPencil = true;
+        takenPencil = true; // pencil has been taken
     }
+
     public synchronized void drop() {
-        takenPencil = false;
-        notifyAll();
+        takenPencil = false; // pencil has been dropped
+        notifyAll(); // notify all other threads
     }
 }
 
-class Eidograph{
-    private boolean takenEido = false;
+// Eidograph class and its synchronization
+class Eidograph {
+    private boolean takenEido = false; // indicating eidograph state (taken or not)
     public synchronized void take() throws InterruptedException {
         while (takenEido) {
-            wait();
+            wait(); // taking some time to get access
         }
-        takenEido = true;
+        takenEido = true; // Eidograph has been taken
     }
+
     public synchronized void drop() {
-        takenEido = false;
-        notifyAll();
+        takenEido = false; // eidograph has been dropped
+        notifyAll(); // notify all other threads
     }
 }
 
 public class Engineers {
 
     public static void main(String[] args) throws Exception {
-        int ponder = 1;
-        int size = 8;
-        ExecutorService exec = Executors.newCachedThreadPool();
-        Pencil[] pencil = new Pencil[size/2];
-        Eidograph[] eidograph = new Eidograph[size/2];
+        // Main program
+        int ponder = 1; // pause factor (defaul0t = 1)
+        int size = 8; // number of copyists
+        ExecutorService exec = Executors.newCachedThreadPool(); // Create a Thread Pool
+        Pencil[] pencil = new Pencil[size / 2]; // Create indicated number of pencil objects
+        Eidograph[] eidograph = new Eidograph[size / 2]; // Create indicated number of eidograph objects
         for (int i = 0; i < 4; i++) {
             pencil[i] = new Pencil();
             eidograph[i] = new Eidograph();
         }
-        
-//        for (int i = 0; i < size; i++){
-//            if ((i % 2) == 0) {
-//                if (i < (size - 1)){
-//                    exec.execute(new Copyist(pencil[i], eidograph[i + 1], i, ponder));
-//                }else{
-//                    exec.execute(new Copyist(pencil[i], eidograph[i + 1], i, ponder));
-//                }
-//            } else {
-//                exec.execute(new Copyist(pencil[0], eidograph[i], i, ponder));
-//            }
-//        }
-        
-        exec.execute(new Copyist(pencil[3], eidograph[0], 0, ponder));
-        exec.execute(new Copyist(pencil[0], eidograph[0], 1, ponder));
-        exec.execute(new Copyist(pencil[0], eidograph[1], 2, ponder));
-        exec.execute(new Copyist(pencil[1], eidograph[1], 3, ponder));
-        exec.execute(new Copyist(pencil[1], eidograph[2], 4, ponder));
-        exec.execute(new Copyist(pencil[2], eidograph[2], 5, ponder));
-        exec.execute(new Copyist(pencil[2], eidograph[3], 6, ponder));
-        exec.execute(new Copyist(pencil[3], eidograph[3], 7, ponder));
-        
+        // Create indicated number of copyist under construction
+        for (int i = 0; i < size; i++) {
+            if (i > 0) {
+                exec.execute(new Copyist(pencil[(i - 1) / 2], eidograph[i / 2], i, ponder));
+                // the pencil & eidograph number has some relationship with copyist number
+            } else {
+                exec.execute(new Copyist(pencil[3], eidograph[i / 2], i, ponder));
+                // but no.0 copyist has no.3 pencil and no.1 eidograph
+            }
+        }
         TimeUnit.SECONDS.sleep(5);
-        exec.shutdownNow();
+        exec.shutdownNow(); // shut down thread pool
     }
 }
